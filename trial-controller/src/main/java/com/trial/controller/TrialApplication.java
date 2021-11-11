46242f1706f895b321d74992b7af59984946b8ad
a9278458 <lzw@haitun.group>
2021-11-12 02:23:10 +0800

    提交openfegin的引用

--- trial-controller/src/main/java/com/trial/controller/TrialApplication.java
+++ trial-controller/src/main/java/com/trial/controller/TrialApplication.java
@@ -3,6 +3,7 @@ package com.trial.controller;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @description: 启动类
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(scanBasePackages={"com.trial.*"})
 @MapperScan("com.trial.mapper")
+@EnableFeignClients
 public class TrialApplication {
 
     public static void main(String[] args) {
